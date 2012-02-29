835a6e3203279d1fe90a2a95516d433fb18fef83
Mathieu Virbel <mat@kivy.org>
2012-02-29 18:06:05 +0100

    add viewport gathering + special enable call for flip

--- kivy/core/window/window_fake.py
+++ kivy/core/window/window_fake.py
@@ -7,3 +7,16 @@ class WindowFake(WindowBase):
     def mainloop(self):
         while not EventLoop.quit and EventLoop.status == 'started':
             EventLoop.idle()
+
+    def create_window(self):
+        from kivy.core.gl import glGetIntegerv, GL_VIEWPORT
+        viewport = glGetIntegerv(GL_VIEWPORT)
+        print 'VIEWPORT===>', viewport
+        self._size = viewport[2:]
+        super(WindowFake, self).create_window()
+        self.dispatch('on_resize', *self._size)
+
+    def flip(self):
+        from kivy.core.gl import glEnable
+        # act as a frame flip trigger for hijacking lib.
+        glEnable(0x9999)
